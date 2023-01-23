92060a4bf23d4f17349eecc630250197d5e8a49d
Miguel Arcanjo <gmrziko82@gmail.com>
2023-01-23 18:20:06 -0300

    1.0

---
+++ day76/main.js
@@ -0,0 +1,9 @@
+// const senha1 = document.querySelector('.senha');
+// const senhaTexto = senha1.value;
+
+
+
+
+// if (senhaTexto === "password") {
+//     senha1.style.backgroundColor = "#64C760";
+// }
