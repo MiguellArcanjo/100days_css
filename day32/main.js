4603e04a6145727ab7f58c354f350b2569ec3cad
Miguel Arcanjo <gmrziko82@gmail.com>
2023-01-23 01:02:41 -0300

    finish

--- day32/main.js
+++ day32/main.js
@@ -1,3 +1,38 @@
-const numero = document.querySelector('numero');
+const numero = document.querySelector('.numero');
 const mais = document.querySelector('.mais');
-const menos = document.querySelector('menos');
+const menos = document.querySelector('.menos');
+
+let contador = 0
+numero.innerHTML = `${contador}`
+
+
+
+
+const cric = () => {
+
+    if (! numero.classList.contains('sobe')) {
+        numero.classList.add('sobe')
+    }
+    
+    setTimeout(() => {
+        contador ++
+        numero.innerHTML = `${contador}`
+
+        numero.classList.remove('sobe')
+    }, 200)
+
+}
+    
+
+const cric2 = () => {
+    if (! numero.classList.contains('desce')) {
+        numero.classList.add('desce')
+    }
+    
+    setTimeout(() => {
+        contador --
+        numero.innerHTML = `${contador}`
+
+        numero.classList.remove('desce')
+    }, 200)
+}
